8eed41abad7f7574b1ec183d96f09d9a50743ca6
Abhishek Shree <abhi1234456@gmail.com>
2021-03-22 22:55:31 +0530

    Webpage enhancement

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,5 +1,6 @@
-const projectsData = [
-  {
+/*
+EXAMPLE:
+{
     title: 'A Search Engine',
     description: `What is you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
@@ -15,14 +16,8 @@ const projectsData = [
     imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
   },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
-]
+*/
+
+const projectsData = []
 
 export default projectsData
