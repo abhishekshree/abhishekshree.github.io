2dd761b49872e4e025ace5c44ad326fc3d350516
Abhishek Shree <abhi1234456@gmail.com>
2021-03-22 22:58:37 +0530

    Added home button

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,4 +1,5 @@
 const headerNavLinks = [
+  { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
   // { href: '/latest', title: 'Latest Posts' },
