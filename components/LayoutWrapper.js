73dd00836d2f538a897cf72ea03286f44948a677
Abhishek Shree <abhi1234456@gmail.com>
2021-05-22 03:30:16 +0530

    Nav Fixed

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -34,9 +34,9 @@ const LayoutWrapper = ({ children }) => {
                 <Link
                   key={link.title}
                   href={link.href}
-                  className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100 strikethrough"
+                  className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100"
                 >
-                  {link.title}
+                  <span className="strikethrough">{link.title}</span>
                 </Link>
               ))}
             </div>
