6769c219600ae056828be0810c5a33ca0c09053a
Abhishek Shree <abhi1234456@gmail.com>
2021-07-19 01:12:07 +0530

    eslint ignore

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ module.exports = withBundleAnalyzer({
   experimental: {
     modern: true,
   },
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack: (config, { dev, isServer }) => {
     config.module.rules.push({
       test: /\.(png|jpe?g|gif|mp4)$/i,
