0bf8b387f5b8d57434dce89bdde017fa8634f261
Abhishek Shree <abhi1234456@gmail.com>
2023-08-08 02:30:55 +0530

    cv

--- pages/cv.js
+++ pages/cv.js
@@ -1,33 +1,14 @@
-import fs from 'fs'
-import path from 'path'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-export async function getServerSideProps(context) {
-  try {
-    // Get the absolute path to your PDF file
-    const pdfPath = path.join(process.cwd(), 'static', 'cv', 'cv.pdf')
+const cv = () => {
+  const router = useRouter()
 
-    // Read the PDF file
-    const pdfBuffer = fs.readFileSync(pdfPath)
+  useEffect(() => {
+    router.replace('/static/cv/cv.pdf')
+  }, [router])
 
-    // Set the appropriate headers for serving the PDF
-    context.res.setHeader('Content-Type', 'application/pdf')
-    context.res.setHeader('Content-Disposition', 'inline; filename=cv.pdf')
-
-    // Send the PDF buffer as the response
-    context.res.statusCode = 200
-    context.res.end(pdfBuffer)
-  } catch (error) {
-    console.error('Error serving CV:', error)
-    context.res.statusCode = 500
-    context.res.end()
-  }
-
-  return {
-    props: {},
-  }
-}
-
-export default function cv() {
-  // This component doesn't need to do anything
   return null
 }
+
+export default cv
