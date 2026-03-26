6a1ff4e70ca9d7096472e8dc3d2745c2bfd6db74
Abhishek Shree <abhishek.shree@outlook.com>
2026-03-26 23:04:48 +0530

    unc age

--- pages/index.js
+++ pages/index.js
@@ -24,12 +24,16 @@ export default function About({ posts }) {
       <div className="divide-y">
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center pt-8 space-x-2">
-            <img src={siteMetadata.image} alt="avatar" className="w-48 h-48 rounded-full" />
+            <img
+              src={siteMetadata.image}
+              alt="avatar"
+              className="w-60 h-60 rounded-full object-cover shadow-xl ring-2 ring-gray-200 dark:ring-gray-800"
+            />
             <h3 className="pt-4 pb-2 mono text-2xl font-bold leading-8 tracking-tight">
               {siteMetadata.author}
             </h3>
             <div className="text-gray-500 mono text-xs dark:text-gray-400">
-              Quant Tech at Qube
+              Quant Technologist at QRT
             </div>
             <div className="text-gray-500 mono text-xs dark:text-gray-400">
               <a
@@ -51,28 +55,24 @@ export default function About({ posts }) {
             </div>
           </div>
           <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">
-            <h1 className="mono text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            <h1 className="serif text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-5xl md:leading-14">
               Namaste!
             </h1>
             <p>
-              I am Abhishek Shree, recently graduated from the Indian Institute of Technology Kanpur. My
-              interests are manifold and are constantly evolving, I take a particular interest 
-              in Programming Languages, Systems, and Quantitative Finance.
+              I am Abhishek Shree, a graduate from the Indian Institute of Technology Kanpur. My
+              interests are manifold and are constantly evolving, I take a particular interest
+              in Programming Languages, Systems and Quantitative Finance. I work at QRT now, helping them
+              trade more and more markets, while learning a lot about markets through the process. I also find myself
+              use a lot of Rust recently, which I find interesting. Free free to drop a Hi if you want to chat about any of these!
             </p>
             <p>
-              When not doing any of those I find myself dabbling around math (mostly abstract) through my coursework
-              or solving some puzzles here and there. I also have developed an appreciation for blogs and podcasts
-              pertaining to tech, geopolitics, and stories around markets recently.
-            </p>
-            <p>
-              Apart from those, I tend to consume a lot of pop culture and read, at a concerningly fast pace at times.
-              I also love to travel and click abstract photos every now and then. If not that, I can also 
-              be found solving my Rubik's cube or vibing{' '}
-              <Link href="https://open.spotify.com/playlist/37i9dQZF1DXaQm3ZVg9Z2X">to this.</Link>
+              In my past life, I used to dabble around math (mostly abstract) through my coursework
+              or solving some puzzles here and there. Apart from those, I tend to consume a lot of pop culture and read, at a concerningly fast pace.
+              I also love to travel and click abstract photos every now and then.
             </p>
             <div className="grid md:grid-cols-2 grid-cols-1">
               <div className="mr-5">
-                <p className="antialiased font-black text-lg leading-tight"> Recently I wrote</p>
+                <p className="antialiased font-bold text-lg leading-tight uppercase tracking-wide text-gray-500"> Recently I wrote</p>
                 {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
                   const { slug, date, title, summary, tags } = frontMatter
                   return (
@@ -103,7 +103,7 @@ export default function About({ posts }) {
               </div>
               <div>
                 <div className="leading-none">
-                  <p className="antialiased font-black text-lg leading-tight"> Education </p>
+                  <p className="antialiased font-bold text-lg leading-tight uppercase tracking-wide text-gray-500"> Education </p>
                 </div>
                 <div className="">
                   <div>
@@ -117,14 +117,14 @@ export default function About({ posts }) {
                 </div>
                 <br />
                 <div>
-                  <div className="leading-tight font-semibold">AISSCE – XII, 2020</div>
+                  <div className="leading-tight font-semibold">AISSCE - XII, 2020</div>
                   <div className="leading-none text-sm text-gray-500">
                     Loyola High School, Patna
                   </div>
                 </div>
                 <br />
                 <div>
-                  <div className="leading-tight font-semibold ">CBSE – X, 2018</div>
+                  <div className="leading-tight font-semibold ">CBSE - X, 2018</div>
                   <div className="leading-none text-sm text-gray-500">
                     Loyola High School, Patna
                   </div>
